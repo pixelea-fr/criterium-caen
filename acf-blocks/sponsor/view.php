e264762fec57b0bf5c0fad79bd08d7be95d793cf
Tanguy Pinel <tanguypinel@gmail.com>
2023-09-18 10:07:55 +0200

    Sponsor

--- acf-blocks/sponsor/view.php
+++ acf-blocks/sponsor/view.php
@@ -3,5 +3,15 @@
     extract($fields);
     ?>
     <section class="sponsor">
-        
+        <div class="content">
+            <figure class="logo-spon">
+                <?php echo wp_get_attachment_image($logo_sponsor, 'full'); ?>
+            </figure>
+            <div class="texte">
+                <?php echo $texte; ?>
+            </div>
+            <figure class="ffc">
+                <?php echo wp_get_attachment_image($logo_ffc, 'full'); ?>
+            </figure>
+        </div>
     </section>
