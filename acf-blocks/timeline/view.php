f064b366de409373677af879296dd8810eacc83b
Tanguy Pinel <tanguypinel@gmail.com>
2023-09-25 11:52:38 +0200

    TimeLine

--- acf-blocks/timeline/view.php
+++ acf-blocks/timeline/view.php
@@ -1,9 +1,20 @@
-    <?php
-    $fields = get_fields();
-    extract($fields);
-    ?>
-    <section class="timeline">
-        <div class="content">
-            
+<?php
+$fields = get_fields();
+extract($fields);
+?>
+<section class="timeline">
+    <div class="content">
+        <?php foreach ($timeline as $t) : extract($t); ?>
+        <div class="item">
+            <div class="text">
+            <h3><?php echo $date; ?></h3>
+            <p class="sous-titre"><?php echo $sous_titre; ?></p>
+            <span><?php echo $zone_de_texte; ?></span>
+            </div>
+            <figure>
+                <?php echo wp_get_attachment_image($image, 'full'); ?>
+            </figure>
         </div>
-    </section>
+        <?php endforeach; ?>
+    </div>
+</section>
