4a702636c75050590713052e8b4b5ad67271bff6
Benjamin Gosset <benjamin.gosset@pixelea.fr>
2023-10-13 16:07:22 +0200

    Page et actualites

--- acf-blocks/actualites/view.php
+++ acf-blocks/actualites/view.php
@@ -2,12 +2,20 @@
 $fields = get_fields();
 extract($fields);
 
-$affichage_titre = $fields['affichage_titre'];
-$nb_articles = $fields['nombre_darticles'];
+$affichage_titre        = $fields['affichage_titre'];
+$presentation_etendue   = $fields['presentation_etendue'];
+$grid_class             = "";
+
+if ( $presentation_etendue == true ) {
+    $nb_articles    = 10;
+    $grid_class     = "long-list";
+} else {
+    $nb_articles    = 4;
+}
 ?>
 <section class="actualites">
     <?php
-        if ( $affichage_titre === 1 ) {
+        if ( $affichage_titre == true ) {
             ?>
                 <h2>Actualites</h2>
             <?php
@@ -23,13 +31,13 @@ $nb_articles = $fields['nombre_darticles'];
         $query = new WP_Query($args);
 
         if ($query->have_posts()) {
-            echo '<div class="articles">';
+            echo '<div class="articles ' . $grid_class . '">';
             while ($query->have_posts()) {
                 $query->the_post();
                 echo '<article><a href="' . get_permalink() . '">';
                 echo '<figure>';
                 if (has_post_thumbnail()) {
-                    the_post_thumbnail('medium');
+                    the_post_thumbnail('large');
                 }
                 echo '</figure>';
                 echo '<div class="text"><h3>' . get_the_title() . '</h3>';
--- single.php
+++ single.php
@@ -1,9 +1,19 @@
-<?php 
+<?php
 
 get_header();
 
-the_title();
 
-the_content();
-
+?>
+<div class="entry-content">
+    <?php
+        the_title( '<h1>', '</h1>', true );
+        ?>
+        <figure>
+            <?php the_post_thumbnail( 'full'); ?>
+        </figure>
+        <?php
+        the_content();
+    ?>
+</div>
+<?php
 get_footer();
