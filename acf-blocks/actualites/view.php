62f6fe1c9609894d54ac9d7aa12a0b851d78f328
Benjamin Gosset <benjamin.gosset@pixelea.fr>
2023-10-13 09:38:59 +0200

    Actualites

--- acf-blocks/actualites/view.php
+++ acf-blocks/actualites/view.php
@@ -1,35 +1,44 @@
+<?php
+$fields = get_fields();
+extract($fields);
+
+$affichage_titre = $fields['affichage_titre'];
+$nb_articles = $fields['nombre_darticles'];
+?>
+<section class="actualites">
     <?php
-    // $fields = get_fields();
-    // extract($fields);
-    ?>
-    <section class="actualites">
-        <h2>Actualites</h2>
-        <div class="content">
+        if ( $affichage_titre === 1 ) {
+            ?>
+                <h2>Actualites</h2>
             <?php
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => 4,
-            );
+        }
+    ?>
+    <div class="content">
+        <?php
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => $nb_articles,
+        );
 
-            $query = new WP_Query($args);
+        $query = new WP_Query($args);
 
-            if ($query->have_posts()) {
-                echo '<div class="articles">';
-                while ($query->have_posts()) {
-                    $query->the_post();
-                    echo '<article><a href="' . get_permalink() . '">';
-                    echo '<figure>';
-                    if (has_post_thumbnail()) {
-                        the_post_thumbnail('medium');
-                    }
-                    echo '</figure>';
-                    echo '<div class="text"><h3>' . get_the_title() . '</h3>';
-                    echo '<p class="exep">' . get_the_excerpt() . '</p></div>';
-                    echo '</a></article>';
+        if ($query->have_posts()) {
+            echo '<div class="articles">';
+            while ($query->have_posts()) {
+                $query->the_post();
+                echo '<article><a href="' . get_permalink() . '">';
+                echo '<figure>';
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail('medium');
                 }
-                echo '</div>';
+                echo '</figure>';
+                echo '<div class="text"><h3>' . get_the_title() . '</h3>';
+                echo '<p class="exep">' . get_the_excerpt() . '</p></div>';
+                echo '</a></article>';
             }
+            echo '</div>';
+        }
 
-            wp_reset_postdata(); ?>
-        </div>
-    </section>
+        wp_reset_postdata(); ?>
+    </div>
+</section>
